3cb1e3802de8e0cf878b4547f160d281fe5ee389
Shaun OConnell <shaunoconnell@gmail.com>
2011-04-05 23:44:09 -0400

    update

--- go.rb
+++ go.rb
@@ -4,14 +4,6 @@ puts "attempting to connect to s3"
 
 require 'aws/s3'
 
-# s3_key_id = "AKIAJRRXVOQBX7AVKBNQ"
-# s3_password = "jPPJV8FqGate+UmoICBNQL8qcRGxiOmtq22soedu"
-# 
-# AWS::S3::Base.establish_connection!(
-#     :access_key_id     => s3_key_id,
-#     :secret_access_key => s3_password
-# )
-
 AWS::S3::Base.establish_connection!(
      :access_key_id     => ENV['AMAZON_ACCESS_KEY_ID'],
      :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
